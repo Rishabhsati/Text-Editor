0da3afa8d6c6a9c224fdd2043fe08f6798fbff1b
Rishabh sati <rishabhsati6505@gmail.com>
2024-01-11 19:49:56 +0530

    Added some comments and format the code

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -3,51 +3,73 @@
 import javax.swing.*;
 public class TextEditor{
 
+    // frame of the text editor 
     JFrame frame;
-    JMenuBar mb;
-    JMenu file,edit;
-    JMenuItem newWindow,open,save,cut,copy,paste,selectAll,close;
-    JTextArea ta;
+    // menu bar 
+    JMenuBar menuBar;
+    // menu item file 
+    JMenu file;
+    // menu items of file
+    JMenuItem newFile,openFile,saveFile;
+    //  menu item edit
+    JMenu edit;
+    // menu item of edit
+    JMenuItem cut,copy,paste,selectAll,close;
+    // text area 
+    JTextArea textArea;
 
     TextEditor(){
+        // Initialize frame
         frame = new JFrame("Text Editor");
-        mb = new JMenuBar();
+        // Initialize menu bar
+        menuBar = new JMenuBar();
+        // Initialize menus 
         file = new JMenu("File");
         edit = new JMenu("Edit");
-
-        newWindow = new JMenuItem("New Window");
-        open = new JMenuItem("Open");
-        save = new JMenuItem("Save");
-
-        file.add(newWindow);
-        file.add(open);
-        file.add(save);
-
+        
+        // Initialize menu item of file
+        newFile= new JMenuItem("New File");
+        openFile = new JMenuItem("Open File");
+        saveFile = new JMenuItem("Save File");
+        
+        // adding file items in the file
+        file.add(newFile);
+        file.add(openFile);
+        file.add(saveFile);
+        
+        // Initialize menu item of edit
         cut = new JMenuItem("Cut");
         copy = new JMenuItem("Copy");
         paste = new JMenuItem("Paste");
         selectAll = new JMenuItem("Select All");
         close = new JMenuItem("Close");
-
+        
+        // adding file items in the edit
         edit.add(cut);
         edit.add(copy);
         edit.add(paste);
         edit.add(selectAll);
         edit.add(close);
 
-        mb.add(file);
-        mb.add(edit);
+        // adding file and edit in the menu bar 
+        menuBar.add(file);
+        menuBar.add(edit);
 
-
-        ta = new JTextArea();
-        ta.setBounds(3,3,500,500);
-        frame.add(mb);
-        frame.add(ta);
-        frame.setJMenuBar(mb);
+        // Initialize text area 
+        textArea = new JTextArea();
+        // set the layout bound of text area
+        textArea.setBounds(0,0,500,500);
         
-        frame.setLayout(null);
+        // adding menubar to the frame 
+        frame.add(menuBar);
+        // adding text area to the frame 
+        frame.add(textArea);
+
+        frame.setJMenuBar(menuBar);
+        frame.setBounds(100,100,400,400);
+        
         frame.setVisible(true);
-        frame.setSize(500,500);
+        frame.setLayout(null);
     }
 
     
